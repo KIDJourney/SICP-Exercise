4abdd09f9c801041095c6cf126c3f2f051909490
KIDJourney <kingdeadfish@qq.com>
2016-03-29 11:27:29 +0800

    add example code

---
+++ chapter1/smallest_divisor.rkt
@@ -0,0 +1,14 @@
+#lang scheme
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (* test-divisor test-divisor) n) n)
+        ((divides? n test-divisor) test-divisor)
+        (else (find-divisor n (+ 1 test-divisor)))))
+
+(define (divides? a b)
+  (= (remainder a b) 0))
+
+(define (primer? n)
+  (= n (smallest-divisor n)))
