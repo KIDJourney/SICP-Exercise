6e5587e7c7f3c9e42770943f8bb566a182dfdeb1
KIDJourney <kingdeadfish@qq.com>
2016-04-05 22:00:33 +0800

    update

--- chapter1/common.rkt
+++ chapter1/common.rkt
@@ -1,7 +1,19 @@
 #lang scheme
-(provide square even?)
+(provide square even? average close-enough?)
 (define (even? x)
   (= (remainder x 2) 0))
 
 (define (square x)
   (* x x))
+
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (abs x)
+  (if (> x 0)
+      x
+      (- 0 x)))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.0001))
