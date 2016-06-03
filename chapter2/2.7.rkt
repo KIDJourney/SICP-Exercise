a7317b17bb149e388ef3c5f7b0f87d9bbde89995
KIDJourney <kingdeadfish@qq.com>
2016-06-03 13:42:08 +0800

    update

---
+++ chapter2/2.7.rkt
@@ -0,0 +1,30 @@
+#lang scheme
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+(define (make-interval x y)
+  (cons x y))
+
+(define (lower-bound x)
+  (min (car x) (cdr x)))
+(define (upper-bound x)
+  (max (car x) (cdr x)))
+
+(define (sub-interval source sub)
+  (cond [(= (lower-bound sub) (lower-bound source))
+         (make-interval (upper-bound sub) (lower-bound source))]
+        [else (make-interval (lower-bound source) (lower-bound sub))]))
