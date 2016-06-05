7f7737f5d90d97ae726a03a72560be45477cdd20
KIDJourney <kingdeadfish@qq.com>
2016-06-05 14:31:11 +0800

    update

---
+++ chapter2/cons_closure.rkt
@@ -0,0 +1,27 @@
+#lang scheme
+(define o2f (list 1 2 3 4))
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
+
+(define (length2 items)
+  (define (length-iter a count)
+    (if (null? a)
+        count
+        (length-iter (cdr a) (+ 1 count))))
+  (length-iter items 0)
+  )
+
+(define (append source ext)
+  (if (null? source)
+      ext
+      (cons (car source) (append (cdr source) ext ))))
+      
+      
