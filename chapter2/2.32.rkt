65a058a892cdcf3f35a0375f0c32ac21d2f031b0
王思宇 <siyu.wang@ele.me>
2016-09-06 11:29:51 +0800

    update

--- chapter2/2.32.rkt
+++ chapter2/2.32.rkt
@@ -3,7 +3,8 @@
   (if (null? s)
       (list '())
       (let ((rest (subsets (cdr s))))
-        (append rest (map subsets rest)))))
+        (append rest (map (lambda (x)
+                            (cons (car s) x))
+                            rest)))))
 
-
-(subsets (list 1 2 3 4))
+(subsets (list 1 2 3 4 ))
