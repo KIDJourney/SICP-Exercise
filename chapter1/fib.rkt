661f6d6ede1cb3bf4ad88e56c3fafee121365dfd
王思宇 <siyu.wang@ele.me>
2016-09-06 21:05:45 +0800

    remove wrong code

--- chapter1/fib.rkt
+++ chapter1/fib.rkt
@@ -1,14 +1,5 @@
 #lang scheme
 
-(define (fib n)
-  (define (fib-iter a b)
-    (- n 1)
-    (if (= n 0)
-        a
-        (fib-iter b (+ a b))))
-  (fib-iter 0 1)
-  )
-
 (define (fib2 n)
   (let loop
     ((m 0)
