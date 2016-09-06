e176ba17d883859d61c42cc429d1d0d343b61ff6
王思宇 <siyu.wang@ele.me>
2016-09-06 11:11:51 +0800

    update

--- chapter2/2.30.rkt
+++ chapter2/2.30.rkt
@@ -8,4 +8,19 @@
          ((not (pair? tree)) (square tree))
          (else (cons (square-tree (car tree))
                      (square-tree (cdr tree))))))
-          
+
+
+(define (tree-operator tree operator)
+  (map operator tree))
+
+(define (square2 tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (* tree tree))
+        (else (cons (square2 (car tree))
+                    (square2 (cdr tree))))))
+
+
+(tree-operator
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)) square2)
--- chapter2/2.31.rkt
+++ chapter2/2.31.rkt
@@ -1,1 +1,18 @@
 #lang scheme
+(define (square x)
+  (* x x))
+(define (square-tree tree)
+  (tree-map square tree))
+
+(define (tree-map operator tree)
+  (define (inner_map item)
+    (cond ((null? item) '())
+          ((not (pair? item)) (operator item))
+          (else (cons (inner_map (car item))
+                      (inner_map (cdr item))))))
+  (inner_map tree))
+
+
+(define my-tree (list 1 (list 2 (list 3 4) 5) (list 6 7 )))
+
+(square-tree my-tree)
